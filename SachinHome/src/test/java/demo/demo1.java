53d3b01661e03c520aae469dafa5a9cc12a10334
Rohan Wagh <rohan.vjti@gmail.com>
2019-09-22 16:44:47 +0530

    1

---
+++ SachinHome/src/test/java/demo/demo1.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+@Test
+public class demo1 {
+
+	public void TestDemo()
+	{
+		System.out.println("HI");// TODO Auto-generated method stub
+
+	}
+
+}
